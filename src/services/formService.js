b57aea35331ca2f07ad7cf4551f5c5b1fc94c6fa
Maksym0509 <maksym.dev0509@gmail.com>
2023-10-08 18:48:45 +0300

    Added the authorization in header info

--- src/services/formService.js
+++ src/services/formService.js
@@ -6,31 +6,57 @@ const API_URL = `/api/form`;
 const config = {
   headers: {
     "Content-Type": "application/json",
+    Authorization: localStorage.getItem('token')
   },
 };
 
 const getTables = async (data) => {
-  const response = await axios.post(`${API_URL}/getTables`, data, config);
+  const response = await axios.post(`${API_URL}/getTables`, data, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem('token')
+    },
+  });
   return response.data;
 };
 
 const getDatabase = async (data) => {
-  const response = await axios.post(`${API_URL}/getDatabase`, data, config);
+  const response = await axios.post(`${API_URL}/getDatabase`, data, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem('token')
+    },
+  });
   return response.data;
 };
 
 const save = async (data) => {
-  const response = await axios.post(`${API_URL}/save`, data, config);
+  const response = await axios.post(`${API_URL}/save`, data, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem('token')
+    },
+  });
   return response.data;
 };
 
 const publish = async (data) => {
-  const response = await axios.post(`${API_URL}/publish`, data, config);
+  const response = await axios.post(`${API_URL}/publish`, data, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem('token')
+    },
+  });
   return response.data;
 };
 
 const getForm = async (data) => {
-  const response = await axios.post(`${API_URL}/getForm`, data, config);
+  const response = await axios.post(`${API_URL}/getForm`, data, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem('token')
+    },
+  });
   return response.data;
 };
 
